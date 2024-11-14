ef6e85492db76e8f8715aac598190947ebf4dffe
Chrystalkey <schaefer10056@gmail.com>
2024-11-14 13:50:33 +0100

    extraction of links in vorgangsseite

--- collector/scrapers/by_lt.py
+++ collector/scrapers/by_lt.py
@@ -58,8 +58,10 @@ def extract_bylt_resultpage(url: str):
 classes:
 - initiativdrucksache
 - stellungnahme
-- plenumsdiskussion
 - ausschussbericht
+- plenumsdiskussion-uebrw
+- plenumsdiskussion-zustm
+- plenumsdiskussion-ablng
 - plenumsbeschluss
 - gesetzesblatt
 - unclassified
@@ -71,7 +73,15 @@ def classify_gridcell(cellsoup: BeautifulSoup) -> str:
     elif cellsoup.text.find("Schriftliche Stellungnahmen im Gesetzgebungsverfahren")  != -1:
         return "stellungnahme"
     elif cellsoup.text.find("Plenum") != -1 and cellsoup.text.find("Plenarprotokoll") != -1:
-        return "plenumsdiskussion"
+        if cellsoup.text.find("Überweisung") != -1:
+            return "plenumsdiskussion-uebrw"
+        elif cellsoup.text.find("Zustimmung") != -1:
+            return "plenumsdiskussion-zustm"
+        elif cellsoup.text.find("Ablehnung") != -1:
+            return "plenumsdiskussion-ablng"
+        else:
+            print(f"Warning: Plenumsdiskussion without specific classification: `{cellsoup}`")
+            return "unclassified"
     elif cellsoup.text.find("Plenum") != -1 and cellsoup.text.find("Plenarprotokoll") == -1:
         return "plenumsbeschluss"
     elif cellsoup.text.find("Ausschuss") != -1:
@@ -81,7 +91,7 @@ def classify_gridcell(cellsoup: BeautifulSoup) -> str:
     else:
         return "unclassified"
 
-def extract_bylt_vorgangspage(url: str):
+def extract_bylt_vorgangspage(url: str) -> gesetzesvorhaben.Gesetzesvorhaben:
     get_result = requests.get(url)
     soup = BeautifulSoup(get_result.content, "html.parser")
     vorgangs_table = soup.find('tbody', id='vorgangsanzeigedokumente_data')
@@ -91,13 +101,42 @@ def extract_bylt_vorgangspage(url: str):
         if len(cells) < 2:
             print(f"Warning: Unexpectedly found less than two gridcells in: `{row}`")
             continue
-        # print date
+        # date is in the first cell
         timestamp = cells[0].text
+        # content is in the second cell
         cellclass = classify_gridcell(cells[1])
         print(f"Timestamp: {timestamp} Cellclass: {cellclass}")
-
+        match cellclass:
+            case "initiativdrucksache": print(extract_singlelink(cells[1]))
+            case "stellungnahme": print(extract_schrstellung(cells[1]))
+            case "plenumsdiskussion-uebrw": print(extract_plenproto(cells[1]))
+            case "plenumsdiskussion-zustm": print(extract_plenproto(cells[1]))
+            case "plenumsdiskussion-ablng": print(extract_plenproto(cells[1]))
+            case "plenumsbeschluss": print(extract_singlelink(cells[1]))
+            case "ausschussbericht": print(extract_singlelink(cells[1]))
+            case "gesetzesblatt": print(extract_gbl_ausz(cells[1]))
+            case "unclassified": print("Unclassifiable cell")
+    # TODO: put this into a Gesetzesvorhaben object
     print(len(rows))
 
+def extract_singlelink(cellsoup: BeautifulSoup) -> str:
+    return cellsoup.find("a")["href"]
+
+# returns: {"typ": [links, links, links], ...}
+def extract_schrstellung(cellsoup: BeautifulSoup) -> dict:
+    links = cellsoup.findAll("a")
+    return {
+        "lobbyregister": [links[0]["href"]],
+        "stellungnahme": [links[1]["href"]]
+    }
+
+def extract_plenproto(cellsoup: BeautifulSoup) -> str:
+    cellsoup_ptr = cellsoup.find(text="Protokollauszug")
+    cellsoup_ptr = cellsoup_ptr.find_previous("br")
+    return cellsoup_ptr.find_next("a")["href"]
+
+def extract_gbl_ausz(cellsoup: BeautifulSoup) -> str:
+    return cellsoup.findAll("a")[1]["href"]
 
 if __name__ == "__main__":
     #CURRENT_WP = 19
