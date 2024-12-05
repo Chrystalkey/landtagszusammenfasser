bd75bf13c75b8528fa425af6d1b6099f9c14075e
Chrystalkey <schaefer10056@gmail.com>
2024-12-05 22:59:13 +0100

    removed faulty ps lines

--- oapi_gen.sh
+++ oapi_gen.sh
@@ -14,15 +14,6 @@ if [! -d "oapi-generator"]; then
     cd ..
 fi
 
-if (-Not (Test-Path -Path "oapi-generator" -PathType Container)) (
-    echo "Creating oai-generator directory"
-    mkdir -p oapi-generator 
-    cd oapi-generator
-    curl https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh > openapi-generator-cli
-    chmod u+x openapi-generator-cli
-    cd ..
-)
-
 
 ./oapi-generator/openapi-generator-cli generate -g python -i $(pwd)/docs/specs/openapi.yml -o $(pwd)/collector/oapicode
 ./oapi-generator/openapi-generator-cli generate -g rust-axum -i $(pwd)/docs/specs/openapi.yml -o $(pwd)/database/oapicode
