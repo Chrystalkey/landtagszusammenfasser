f6570a2baca66d12d96e4e215ee53a1e1ff38103
Benedikt Schäfer <benediktschaefer@protonmail.com>
2024-12-06 10:56:42 +0100

    fixed bash script directory check

--- oapi_gen.sh
+++ oapi_gen.sh
@@ -3,17 +3,21 @@
 # It will download the openapi-generator-cli if it is not already present
 # The generated code will be placed in the collector/oapicode and database/oapicode directories
 
+DIRECTORY="oapi-generator"
+
 echo "Checking for openapi-generator-cli"
 
-if [! -d "oapi-generator"]; then
-    echo "Creating oai-generator directory"
-    mkdir -p oapi-generator 
-    cd oapi-generator
-    curl https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh > openapi-generator-cli
-    chmod u+x openapi-generator-cli
-    cd ..
+if [ -d "$DIRECTORY" ]; then
+    echo "Directory $DIRECTORY found!"
+else
+    echo "Creating $DIRECTORY directory"
+    #mkdir "$DIRECTORY"
+    #cd "$DIRECTORY"
+    #curl https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh > openapi-generator-cli
+    #chmod u+x openapi-generator-cli
+    #cd ..
 fi
 
 
-./oapi-generator/openapi-generator-cli generate -g python -i $(pwd)/docs/specs/openapi.yml -o $(pwd)/collector/oapicode
-./oapi-generator/openapi-generator-cli generate -g rust-axum -i $(pwd)/docs/specs/openapi.yml -o $(pwd)/database/oapicode
+./$DIRECTORY/openapi-generator-cli generate -g python -i $(pwd)/docs/specs/openapi.yml -o $(pwd)/collector/oapicode
+./$DIRECTORY/openapi-generator-cli generate -g rust-axum -i $(pwd)/docs/specs/openapi.yml -o $(pwd)/database/oapicode
