a81352061dd09ce9fb2ad9afa74eacd6b2b59dd3
Benedikt Schäfer <benediktschaefer@protonmail.com>
2025-01-31 13:47:17 +0100

    added merge testing stub

---
+++ database/tests/merging_tests.rs
@@ -0,0 +1,9 @@
+#![cfg(test)]
+
+#[test]
+fn test_merge_scenarios() {
+    // set up database connection and clear it
+    // for each scenario in the testfiles folder
+    // run the merge functions
+    // check the result
+}
