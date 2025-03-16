a8afb3ca49518fab6b2a7fad04ffec5a20db7814
BTZusFas <hallo@bundestagszusammenfasser.de>
2025-03-16 20:08:55 +0100

    Verbesserungen am BundestagAPIScraper und Aktualisierung der OpenAPI-Spezifikation
    - Hinzufügen von Methoden zur Extraktion von Vorgangspositionen und zur Erstellung von URLs für Gesetzesvorhaben
    - Anpassung der Logik zur Erkennung von Vorgangstypen und Initiatoren
    - Aktualisierung der Stationen-Extraktion basierend auf Vorgangspositionen
    - Erweiterung der OpenAPI-Spezifikation um neue Stationstypen für abgeschlossene Ausschussberatungen und Verzögerungen

--- collector/collector/scrapers/bt_scraper.py
+++ collector/collector/scrapers/bt_scraper.py
@@ -1,9 +1,10 @@
 import logging
 from typing import List, Dict, Any, Optional
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 import uuid
 import aiohttp
 import json
+import re
 import openapi_client.models as models
 from collector.interface import Scraper
 from collector.document import Document
@@ -13,6 +14,9 @@ logger = logging.getLogger(__name__)
 
 
 class BundestagAPIScraper(Scraper):
+    CURRENT_WP = 20
+    BT_API_KEY = "I9FKdCn.hbfefNWCY336dL6x62vfwNKpoN2RZ1gp21"
+
     def __init__(self, config, session: aiohttp.ClientSession):
         listing_urls = ["https://search.dip.bundestag.de/api/v1"]
         super().__init__(config, uuid.uuid4(), listing_urls, session)
@@ -21,27 +25,26 @@ class BundestagAPIScraper(Scraper):
             'api-key': config.api_key
         })
         self.vorgaenge = {}  # Dictionary zum Speichern der Vorgangsdaten
+        self.positionen = {} # Dictionary zum Speichern der Vorgangspositionen
     
     async def listing_page_extractor(self, url) -> list[str]:
         """
         Holt Gesetzesvorhaben von der Bundestags-API
         """
-        CURRENT_WP = 20
-        tage = 30
-        bt_api_key = "I9FKdCn.hbfefNWCY336dL6x62vfwNKpoN2RZ1gp21"
-        #startdatum = datetime.today() - datetime.timedelta(days=tage)
-        startdatum = datetime(2025, 2, 3)
+        tage = 5
+        startdatum = datetime.today() - timedelta(days=tage)
+        #startdatum = datetime(2025, 2, 3)
         startdatum = startdatum.strftime("%Y-%m-%dT00:00:00")
-        #enddatum = datetime.today()
-        enddatum = datetime(2025, 2, 3)
+        enddatum = datetime.today()
+        #enddatum = datetime(2025, 2, 3)
         enddatum = enddatum.strftime("%Y-%m-%dT23:59:59")
         endpoint = f"{self.listing_urls[0]}/vorgang"
         params = {
-            "apikey": bt_api_key,
+            "apikey": self.BT_API_KEY,
             "f.aktualisiert.start" : startdatum,
             "f.aktualisiert.end" : enddatum,
             "f.vorgangstyp" : "Gesetzgebung",
-            "f.wahlperiode" : CURRENT_WP
+            "f.wahlperiode" : self.CURRENT_WP
         }
         
         async with self.session.get(endpoint, params=params) as response:
@@ -57,7 +60,7 @@ class BundestagAPIScraper(Scraper):
                 logger.debug(f"Error while fetching listing page: {await response.text()}")
                 return []
         
-    async def item_extractor(self, vorgang_id: str) -> models.Gesetzesvorhaben:
+    async def item_extractor(self, vorgang_id: str) -> models.Vorgang:
         """
         Transformiert die Bundestags-API-Daten in das Format der OpenAPI-Spezifikation
         """
@@ -66,96 +69,134 @@ class BundestagAPIScraper(Scraper):
             return None
             
         vorgang = self.vorgaenge[vorgang_id]
+        positionen = await self._get_vorgangspositionen(vorgang_id)
+
         # Basis-Gesetzesvorhaben erstellen
-        gsvh = models.Gesetzesvorhaben.from_dict({
+        gsvh = models.Vorgang.from_dict({
             "api_id": str(uuid.uuid4()),
             "titel": vorgang.get("titel", ""),
-            #ToDo: Kurztitel/Offizieller Titel
-            "parlament": "BT", #ToDo: Abhängig von Zuordnung vorgangsposition
-            "verfassungsaendernd": "Grundgesetz" in vorgang.get("titel", ""), #ToDo: FUnktioniert das?
+            "verfassungsaendernd": "Änderung des Grundgesetzes" in vorgang.get("titel", ""),
             "trojaner": False,  
-            "initiatoren": self.extract_initiatoren(vorgang), #ToDo: Checken / Was ist mit den Ministerien?
-            "typ": "bt-parlament",  #ToDo: Erkennung Zustimmung/Einspruchsgesetz
-            "wahlperiode": vorgang.get("wahlperiode", 20),  # Aktuelle Wahlperiode als Standard
+            "initiatoren": self._extract_initiatoren(vorgang), 
+            "typ": self._get_vorgangstyp(vorgang),
+            "wahlperiode": self.CURRENT_WP,  
             "ids": [
-                models.Identifikator.from_dict({
+                models.VgIdent.from_dict({
                     "typ": "vorgnr",
-                    "id": str(vorgang.get("id"))  #ToDo: initdrucks zusätzlich aus vorgangsposition.
+                    "id": str(vorgang.get("id"))
+                }),
+                models.VgIdent.from_dict({
+                    "typ": "initdrucks",
+                    "id": self._get_initdrucks_nummer(positionen)
                 })
             ],
-            "links": [f"https://pdok.bundestag.de/vorgang/{vorgang.get('id')}"],  #ToDo: Korrekten Link generieren
-            "stationen": self.extract_stationen(vorgang) #ToDo: Checken und mappen aus beratungsstand
+            "links": [self._create_dip_url(vorgang.get("id"), vorgang.get("titel"))], 
+            "stationen": self._extract_stationen(positionen) #ToDo: Checken und mappen aus beratungsstand
         })
         
         logger.info(f"Daten: {gsvh}")
         return gsvh
 
-    def extract_initiatoren(self, vorgang: Dict) -> List[str]:
+    async def _get_vorgangspositionen(self, vorgang_id: str) -> List[Dict]:
+        """
+        Holt die Vorgangspositionen zum Vorgang
+        """
+        endpoint = f"{self.listing_urls[0]}/vorgangsposition"
+        params = {
+            "apikey": self.BT_API_KEY,
+            "f.vorgang" : vorgang_id,
+            "f.wahlperiode" : self.CURRENT_WP
+        }
+
+        async with self.session.get(endpoint, params=params) as response:
+            if response.status == 200:
+                data = await response.json()
+                position = data.get("documents", [0])
+                return position
+            else:
+                logger.debug(f"Error while fetching vorgangspositionen: {await response.text()}")
+                return []
+
+    def _get_initdrucks_nummer(self, positionen: List[Dict]) -> str:
+        """
+        Extrahiert die Dokumentennummer aus der Fundstelle für Vorgangspositionen vom Typ 'Gesetzentwurf'
+        
+        """
+        for pos in positionen:
+            if pos.get("vorgangsposition") == "Gesetzentwurf":
+                return pos.get("fundstelle", {}).get("dokumentnummer", "")
+        return ""
+
+    def _get_vorgangstyp(self, vorgang: Dict) -> str:
+        """Erkennung Zustimmung/Einspruchsgesetz"""
+        zustimmungen = vorgang.get("zustimmungsbeduerftigkeit", [])
+        
+        if not zustimmungen:
+            return "sonstig"
+            
+        gefundene_typen = set()
+        
+        for item in zustimmungen:
+            if item.startswith("Nein"):
+                gefundene_typen.add("gg-einspruch")
+            elif item.startswith("Ja"):
+                gefundene_typen.add("gg-zustimmung")
+        
+        # Wenn mehrere unterschiedliche Typen gefunden wurden oder kein Typ erkannt wurde
+        if len(gefundene_typen) != 1:
+            return "sonstig"
+            
+        # Ansonsten den einzigen gefundenen Typ zurückgeben
+        return gefundene_typen.pop()
+    
+    def _extract_initiatoren(self, vorgang: Dict) -> List[str]:
         """Extrahiert die Initiatoren aus den Vorgangsdaten"""
         initiatoren = []
         if vorgang.get("initiative"):
             initiatoren.extend(vorgang["initiative"])
         return initiatoren
 
-    def extract_stationen(self, vorgang: Dict) -> List[models.Station]:
-        """Extrahiert die Stationen aus den Vorgangsdaten"""
+    def _extract_stationen(self, positionen: List[Dict]) -> List[models.Station]:
+        """Extrahiert die Station aus den Vorgangsdaten"""
         stationen = []
         
-        # Beispiel für eine Initiativ-Station
-        if vorgang.get("initiative"):
-            init_station = models.Station.from_dict({
-                "datum": self._parse_date(vorgang.get("initiativDatum")),
-                "gremium": "Bundestag",
-                "dokumente": self._extract_dokumente(vorgang.get("initiativDrucksache")),
-                "link": f"https://pdok.bundestag.de/vorgang/{vorgang.get('vorgangsId')}",
-                "parlament": "BT",
-                "schlagworte": vorgang.get("sachgebiet", []),
-                "stellungnahmen": [],
-                "typ": models.Stationstyp.PARL_MINUS_INITIATIV,
-                "trojaner": False,
-                "betroffene_texte": []
-            })
-            stationen.append(init_station)
-        
-        # Weitere Stationen aus Vorgangsablauf extrahieren
-        for aktivitaet in vorgang.get("vorgangsablauf", []):
-            station = self._create_station_from_aktivitaet(aktivitaet)
+        for position in positionen:
+            station = self._create_station_from_position(position)
             if station:
                 stationen.append(station)
         
         return stationen
 
-    def _create_station_from_aktivitaet(self, aktivitaet: Dict) -> Optional[models.Station]:
+    def _create_station_from_position(self, position: Dict) -> Optional[models.Station]:
         """Erstellt eine Station aus einer Vorgangsaktivität"""
         station_mapping = {
-            "Dem Bundestag zugeleitet - Noch nicht beraten": models.Stationstyp.PARL_MINUS_INITIATIV,
-            "Dem Bundesrat zugeleitet - Noch nicht beraten": models.Stationstyp.PARL_MINUS_INITIATIV,
-            "Noch nicht beraten": models.Stationstyp.PARL_MINUS_INITIATIV,
-            "Überwiesen": models.Stationstyp.PARL_MINUS_AUSSCHBER,
-            "Im Vermittlungsverfahren": models.Stationstyp.PARL_MINUS_AUSSCHBER,
-            "Zustimmung": models.Stationstyp.PARL_MINUS_AKZEPTANZ,
-            "Verkündet": models.Stationstyp.POSTPARL_MINUS_GSBLT,
-            "Für erledigt erklärt": models.Stationstyp.PARL_MINUS_ABLEHNUNG,
-            "Verabschiedet": models.Stationstyp.PARL_MINUS_AKZEPTANZ,
+            "Gesetzentwurf": models.Stationstyp.PARL_MINUS_INITIATIV,
+            "1. Beratung": models.Stationstyp.PARL_MINUS_AUSSCHBER,
+            "Durchgang": models.Stationstyp.PARL_MINUS_AUSSCHBER,
+            "1. Durchgang": models.Stationstyp.PARL_MINUS_AUSSCHBER,
+            "Beschlussempfehlung und Bericht": models.Stationstyp.PARL_MINUS_BERABGESCHL,
+            "Beschlussempfehlung": models.Stationstyp.PARL_MINUS_BERABGESCHL,
+            "Empfehlungen der Ausschüsse": models.Stationstyp.PARL_MINUS_BERABGESCHL,
+            "Bericht gemäß § 96 Geschäftsordnung BT": models.Stationstyp.PARL_MINUS_VERZOEGERT,
+            "2. Beratung": models.Stationstyp.PARL_MINUS_AUSSCHBER,
+            "3. Beratung": models.Stationstyp.PARL_MINUS_AUSSCHBER,
+            "2. Durchgang": models.Stationstyp.PARL_MINUS_AUSSCHBER,
+            
+            
         }
         
-        typ = station_mapping.get(aktivitaet.get("aktivitaetsart"))
+        typ = station_mapping.get(position.get("vorgangsposition"))
         if not typ:
-            return None
+            typ = models.Stationstyp.SONSTIG
             
-        datum = self._parse_date(aktivitaet.get("datum"))
+        datum = self._parse_date(position.get("datum"))
         return models.Station.from_dict({
             "datum": datum,
             "start_zeitpunkt": f"{datum}T00:00:00",  # Startzeitpunkt als Datum mit 00:00:00
-            "gremium": aktivitaet.get("gremium", "Bundestag"),
-            "dokumente": self._extract_dokumente(aktivitaet.get("drucksache")),
-            "link": aktivitaet.get("fundstelle"),
-            "parlament": "BT",
-            "schlagworte": [],
-            "stellungnahmen": [],
+            "dokumente": [],  # Leere Liste als Standardwert, ToDo: siehe Notizen
+            "link": position.get("fundstelle").get("pdf_url"),
+            "parlament": position.get("zuordnung"),
             "typ": typ,
-            "trojaner": False,
-            "betroffene_texte": []
         })
 
     def _extract_dokumente(self, drucksache: Dict) -> List[models.Dokument]:
@@ -174,6 +215,17 @@ class BundestagAPIScraper(Scraper):
             "autorpersonen": [],
             "autoren": []
         })]
+
+    def _create_dip_url(self, vorgangid, titel):
+        #Bildet die URL zum Bundestags DIP aus dem Gesetzestitel
+        cleantitle = re.sub(r"[^a-zA-Z0-9]", "-", titel)
+        cleantitle = re.sub(r"--", "-", cleantitle)
+        cleantitle = cleantitle.lower()
+        cleantitle = cleantitle[:100]
+        
+        url = "https://dip.bundestag.de/vorgang/" + cleantitle + "/" + str(vorgangid)
+        return url
+    
 
     def _parse_date(self, date_str: str) -> str:
         """Konvertiert ein Datum-String in das erwartete ISO-Format"""
