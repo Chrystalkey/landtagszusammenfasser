a59cbb0411e630b53463cbb94fd5b58f31993bc8
Benedikt Schäfer <benediktschaefer@protonmail.com>
2025-03-30 18:41:45 +0200

    branched out collector

--- oapi_gen.ps1
+++ oapi_gen.ps1
@@ -2,24 +2,12 @@
 # It will download the openapi-generator-cli if it is not already present
 # The generated code will be placed in the collector/oapicode and database/oapicode directories
 
-echo "Checking for openapi-generator-cli"
-
-if (-Not (Test-Path -Path "oapi-generator" -PathType Container)) {
-    Write-Host "Creating oapi-generator directory"
-    New-Item -ItemType Directory -Path "oapi-generator" -Force | Out-Null
-    Set-Location -Path "oapi-generator"
-    
-    # Download the openapi-generator-cli script
-    & Invoke-WebRequest -OutFile openapi-generator-cli.jar https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.11.0/openapi-generator-cli-7.11.0.jar
-    
-    Set-Location -Path ".."
-}
-
-
-# Generate Python client code
-& java -jar "./oapi-generator/openapi-generator-cli.jar" generate -g python -i "$(Get-Location)/docs/specs/openapi.yml" -o "$(Get-Location)/collector/oapicode"
-# Twice, once for the website and once for the collector
-& java -jar "./oapi-generator/openapi-generator-cli.jar" generate -g python -i "$(Get-Location)/docs/specs/openapi.yml" -o "$(Get-Location)/webserver/oapicode"
-
-# Generate Rust Axum server code
-& java -jar "./oapi-generator/openapi-generator-cli.jar" generate -g rust-axum -i "$(Get-Location)/docs/specs/openapi.yml" -o "$(Get-Location)/database/oapicode"
+echo "[OAPI-Gen] Generating Client and Server Code from Spec"
+Set-Location -Path "ltzf-backend"
+& oapigen.ps1
+Set-Location -Path "../ltzf-collector"
+& oapigen.ps1
+Set-Location -Path "../ltzf-website"
+& oapigen.ps1
+Set-Location -Path ".."
+echo "[OAPI-Gen] Done Generating Client and Server Code"
--- oapi_gen.sh
+++ oapi_gen.sh
@@ -3,24 +3,12 @@
 # It will download the openapi-generator-cli if it is not already present
 # The generated code will be placed in the collector/oapicode and database/oapicode directories
 
-export OPENAPI_GENERATOR_VERSION="7.11.0"
-DIRECTORY="oapi-generator"
-
-echo "Checking for openapi-generator-cli"
-
-if [ -d "$DIRECTORY" ]; then
-    echo "Directory $DIRECTORY found!"
-else
-    echo "Creating $DIRECTORY directory"
-    mkdir "$DIRECTORY"
-    cd "$DIRECTORY"
-    curl https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh > openapi-generator-cli
-    chmod u+x openapi-generator-cli
-    cd ..
-fi
-
-
-OPENAPI_GENERATOR_VERSION="7.11.0" ./$DIRECTORY/openapi-generator-cli generate -g python -i $(pwd)/docs/specs/openapi.yml -o $(pwd)/collector/oapicode
-OPENAPI_GENERATOR_VERSION="7.11.0" ./$DIRECTORY/openapi-generator-cli generate -g python -i $(pwd)/docs/specs/openapi.yml -o $(pwd)/webserver/oapicode
-
-OPENAPI_GENERATOR_VERSION="7.11.0" ./$DIRECTORY/openapi-generator-cli generate -g rust-axum -i $(pwd)/docs/specs/openapi.yml -o $(pwd)/database/oapicode
+echo "[OAPI-Gen] Generating Client and Server Code from Spec"
+cd -Path "ltzf-backend"
+sh oapigen.sh
+cd "../ltzf-collector"
+sh oapigen.sh
+cd -Path "../ltzf-website"
+sh oapigen.sh
+cd ..
+echo "[OAPI-Gen] Done Generating Client and Server Code"
